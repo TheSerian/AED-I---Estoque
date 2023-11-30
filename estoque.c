d9bba280cc4ae05167772d7d54d2fd0feb1935b9
TheSerian <151098795+TheSerian@users.noreply.github.com>
2023-11-30 11:15:43 -0300

    Update estoque.c
    Criação da função informacoesProduto.

--- estoque.c
+++ estoque.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "estoque.h"
 
 #define tamanho_inicial 10
 #define tamanho_incremento 10
+
+void informacoesProduto (Produto *produto){
+  printf("Insira o numero de identificacao do produto: ");
+  scanf("%i", &produto->id);
+  
+  printf("Insira a descricao do produto (max. 50 caracteres):" );
+  getchar();
+  fgets(produto->descricao, 51, stdin);
+  produto->descricao[strcspn(produto->descricao, "\n")] = '\0';
+  
+  printf("Insira a categoria do produto: ");
+  scanf("%i", &produto->categoria);
+  
+  printf("Insira o valor (em reais) do produto: ");
+  scanf("%f", &produto->valor);
+  
+  printf("Insira a quantidade do produto: ");
+  scanf("%i", &produto->quantidade);
+}
 
 int inserirInicio (Produto produto, Estoque *estoque){
   if (estoque->qtdProdutos % tamanho_incremento == 0){
