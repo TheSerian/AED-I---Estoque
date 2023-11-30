d92908ef21be21339b679313131e8e0de3943601
TheSerian <151098795+TheSerian@users.noreply.github.com>
2023-11-30 12:11:46 -0300

    Update estoque.c
    Criação da função inserirFim.

--- estoque.c
+++ estoque.c
@@ -23,6 +23,8 @@ void informacoesProduto (Produto *produto){
   
   printf("Insira a quantidade do produto: ");
   scanf("%i", &produto->quantidade);
+
+  return 1; //produto criado com sucesso
 }
 
 int inserirInicio (Produto produto, Estoque *estoque){
@@ -38,12 +40,24 @@ int inserirInicio (Produto produto, Estoque *estoque){
   }
   estoque->produtos[0] = produto;
   estoque->qtdProdutos++;
+  
   return 1; //insercao bem sucedida
 }
 
+int inserirFim (Produto produto, Estoque *estoque){
+  if (estoque->qtdProdutos % tamanho_incremento == 0){
+    estoque->produtos = realloc(estoque->produtos, estoque->qtdProdutos + tamanho_incremento * sizeof(Produto));
+  }
+  if (estoque->produtos == NULL){
+    printf("Erro de alocacao de memoria.\n");
+    return 0; //alocacao mal sucedida
+  }
+  estoque->produtos[estoque->qtdProdutos] = produto;
+  estoque->qtdProdutos++;
+  
+  return 1; //insercao bem sucedida
+}
 
-
-int inserirFim (Produto produto, Estoque *estoque);
 int inserirPosicao (Produto produto, int posicao, Estoque *estoque);
 void listar (Estoque estoque);
 int removerPosicao (int posicao, Estoque *estoque);
