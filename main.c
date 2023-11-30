38316ec7a7d20ff376d737d3dd8760811e3e9750
TheSerian <151098795+TheSerian@users.noreply.github.com>
2023-11-30 10:48:43 -0300

    Create main.c
    main criada junto do menu de opções.

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "estoque.h"
+
+#define tamanho_inicial 10
+#define tamanho_incremento 10
+
+int main() {
+  Estoque estoque;
+  Produto produto;
+  char opcao;
+  int posicao;
+
+  estoque.produtos = malloc(tamanho_inicial * sizeof(Produto));
+  estoque.qtdProdutos = 0;
+
+  if (estoque.produtos == NULL){
+    printf("Erro de alocacao de memoria.\n");
+    return 1;
+  }
+  do{
+    printf("1. Inserir produto no inicio do estoque.\n");
+    printf("2. Inserir produto no fim do estoque.\n");
+    printf("3. Escolher a posicao na qual o produto sera inserido.\n");
+    printf("4. Listar os produtos no estoque.\n");
+    printf("5. Escolher a posicao na qual o produto sera removido.\n");
+    printf("6. Escolher qual produto sera removido.\n");
+    printf("7. Procurar um produto no estoque.\n");
+    printf("8. Tamanho do estoque.\n");
+    printf("0. Sair do estoque.\n");
+    printf("Escolha a opcao: ");
+    scanf("%c", &opcao);
+    getchar();
+
+    switch(opcao){
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '0':
+      default:
+        printf("Opcao invalida.\n");
+    }
+  }while (opcao != '0');
+
+
+return 0;  
+}
