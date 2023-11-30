64ebe3e4a1618c3ef51ed8f293a1a12ba84278e1
TheSerian <151098795+TheSerian@users.noreply.github.com>
2023-11-30 10:07:52 -0300

    Update estoque.c
    Função inserirInicio foi construída

--- estoque.c
+++ estoque.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "estoque.h"
 
+#define tamanho_inicial 10
+#define tamanho_incremento 10
+
+int inserirInicio (Produto produto, Estoque *estoque){
+  if (estoque->qtdProdutos % tamanho_incremento == 0){
+    estoque->produtos = realloc(estoque->produtos, estoque->qtdProdutos + tamanho_incremento * sizeof(Produto));
+  }
+  if (estoque->produtos == NULL){
+    printf("Erro de alocacao de memoria.\n");
+    return 0; //alocacao mal sucedida
+  }
+  for (int i = estoque->qtdProdutos; i < 0; i--){
+    estoque->produtos [i] = estoque->produtos [i--];
+  }
+  estoque->produtos[0] = produto;
+  estoque->qtdProdutos++;
+  return 1; //insercao bem sucedida
+}
+
+
+
+int inserirFim (Produto produto, Estoque *estoque);
+int inserirPosicao (Produto produto, int posicao, Estoque *estoque);
+void listar (Estoque estoque);
+int removerPosicao (int posicao, Estoque *estoque);
+int removerValor (int id, Estoque *estoque);
+int procurar (int id, Estoque *estoque);
+int tamanho (Estoque *estoque);
